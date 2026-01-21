f2d787453c8cb32960fcca63397d1e6f86165ed7
Nils Hartmann <nils@nilshartmann.net>
2026-01-21 15:41:48 +0100

    Activity-Komponente zum ein- und ausblenden von Komponenten

--- workspace/src/components/App.tsx
+++ workspace/src/components/App.tsx
@@ -1,42 +1,38 @@
 import PlantCard from "./PlantCard.tsx";
 import { Plant } from "../types.ts";
-import PlantCardList from "./PlantCardList.tsx";
 import IntervalSelector from "./IntervalSelector.tsx";
-import { useState } from "react";
+import { Activity, useState } from "react";
+
+const allPlants: Plant[] = [
+  {
+    id: "1",
+    name: "Aloe Vera",
+    location: "Schlafzimmer",
+    wateringInterval: 12,
+    lastWatered: "2025-06-16",
+  },
+  {
+    id: "2",
+    name: "Orchidee",
+    location: "Wohnzimmer",
+    wateringInterval: 20,
+  },
+];
 
 export default function App() {
-
-  const allPlants: Plant[] = [
-    {
-      id: "1",
-      name: "Aloe Vera",
-      location: "Schlafzimmer",
-      wateringInterval: 12,
-      lastWatered: "2025-06-16",
-    },
-    {
-      id: "2",
-      name: "Orchidee",
-      location: "Wohnzimmer",
-      wateringInterval: 20,
-    },
-  ];
-
-  const [isIntervalSelectorOpen, setIsIntervalSelectorOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(true)
 
   return (
     <div className={"AppContainer"}>
       <button
-        onClick={() => setIsIntervalSelectorOpen(!isIntervalSelectorOpen)}
-      >IntervalSelector ein/ausschalten</button>
+        onClick={() => setIsOpen(!isOpen)}>IntervalSelector ein/ausschalten
+      </button>
+
       <form>
-        {isIntervalSelectorOpen ? <IntervalSelector /> : null}
+        <Activity mode={isOpen ? "visible": "hidden"}>
+          <IntervalSelector /> 
+        </Activity>
       </form>
-      {/*<PlantCardList plants={allPlants} />*/}
-      {/*<PlantCard*/}
-      {/*  name="Aloe Vera"*/}
-      {/*  location="Schlafzimmer" wateringInterval={7}*/}
-      {/*/>*/}
     </div>
   );
 }
